dc535999f97344a3f7fec36e835abfa26ef3e4ed
MatheusPMatos <mpeima@yahoo.com.br>
2025-01-25 15:27:58 -0300

    mais informacoes

--- service/MessageHandler/messageHandler.go
+++ service/MessageHandler/messageHandler.go
@@ -3,9 +3,14 @@ package messagehandler
 import "cycleexemplo/service/message"
 
 type messagehandler struct {
-	MsgSV message.Message // USAREI O SERVICO PARA BUSCAR E PERCISTIR MSGS
+	MsgSV     message.Message // USAREI O SERVICO PARA BUSCAR E PERCISTIR MSGS
+	WebSocket string          // servico de sockets para notificar front
 }
 
 type Messagehandler interface {
-	HandleEvents(evt interface{}) //HANDLE QUE SERA PASSADO PARA O CLIENT WHATSAPP ENVIAR OS EVENTOS
+	HandleEvents(evt interface{})
+	//HANDLE QUE SERA PASSADO PARA O CLIENT WHATSAPP ENVIAR OS EVENTOS
+	//Recebe o evento verifica o tipo
+	// ex: tipo.message -> ai chama o msg service para salvar a mensagem
+	// 					-> notifica o front com o websocket passando a nova msg
 }
--- service/WhatsApp/whatsapp.go
+++ service/WhatsApp/whatsapp.go
@@ -5,7 +5,8 @@ import (
 )
 
 type whatsAppservice struct {
-	Clts    map[uint]*Client
+	Clts map[uint]*Client //a lista das conexoes dos diferentes numeros conectados no sistema
+	// conforme a necessidade busco o cliente correspondente para enviar a mensagem
 	Handler messagehandler.Messagehandler //INJETO O HANDLE SERVICE PARA PASSAR A FUNCAO EVENTHANDLER
 }
 
@@ -13,11 +14,14 @@ type WhatsAppservice interface {
 	InitConections()
 }
 
-type Client struct {
+type Client struct { // simula o client whatsapp fornecido pelo package whatsmeow
 	Handler EventHandler
 }
 
 func (c *Client) AddEventHandler(handler EventHandler) {
+	// a tal funcao que o client tem onde passamos a funcao handler que
+	// ira manejar os eventos
+
 	c.Handler = handler
 }
 
